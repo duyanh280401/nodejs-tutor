290f065581725dcfc487deb091f1e8f6ed88cd33
DuyAnhVux <duyanh280401@gmail.com>
2023-07-12 12:06:55 +0700

    day 12-7

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -10,11 +10,43 @@ let getDetailPage = async (req, res) => {
     //logic
     let userId = req.params.id;
     let [user] = await pool.execute('SELECT * FROM users where id=?',[userId]);
-    console.log('info',user);
+    //console.log('info',user);
     return res.send(JSON.stringify(user));
 }
 
+let createNewUser = async (req, res) => {
+    //logic
+    //console.log('check request:', req.body);
+    let {firstName, lastName, email, address} = req.body;
+    await pool.execute('insert into users(firstName,lastName,email,address) values(?,?,?,?)',
+    [ firstName, lastName, email, address]);
+    return res.redirect('/');
+}
+
+let deleteUser = async (req, res) => {
+    //logic
+    let userId = req.body.userId;
+    await pool.execute('delete from users where id=?',[userId]);
+    return res.redirect('/');
+}
+
+let getEditPage = async (req, res) => {
+    //logic
+    let id = req.params.id;
+    let [user] = await pool.execute('SELECT * FROM users where id=?',[id]);
+    return res.render('update.ejs', {dataUser: user[0]});
+}
+
+let updateUser = async (req, res) => {
+    //logic
+    // console.log('check request:', req.body);
+    let {firstName, lastName, email, address,id} = req.body;
+    await pool.execute('update users set firstName=?,lastName=?,email=?,address=? where id =?',
+    [firstName,lastName,email,address,id]);
+    return res.redirect('/');
+}
+
 module.exports = {
-    getHomepage,getDetailPage
+    getHomepage,getDetailPage,deleteUser,createNewUser,getEditPage,updateUser
 }
 
--- src/route/web.js
+++ src/route/web.js
@@ -8,6 +8,14 @@ const initWebRoute = (app) =>{
 
     router.get('/detail/user/:id', homeController.getDetailPage);
 
+    router.post('/create-new-user', homeController.createNewUser);
+
+    router.post('/delete-user', homeController.deleteUser);
+
+    router.get('/edit-user/:id', homeController.getEditPage );
+
+    router.post('/update-user', homeController.updateUser);
+
     router.get('/baby', (req, res) => {
         res.send('Hello my baby!')
     })
